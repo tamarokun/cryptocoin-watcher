07d1a4f6edbc2f1fa0a1d2305e3424152a0f061d
Dusan Tudik <tudik@nama5.com>
2017-07-16 17:52:03 +0200

    profit table WIP with static fake trade data, loading exchange rates from cryptocompare api

---
+++ app/app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var app = angular.module('sciwApp', [
+    'ngTouch'
+]);
+
+app.run(['$rootScope', '$http', function($rootScope, $http)
+{
+    var $s = $rootScope,
+        apiLink = 'https://min-api.cryptocompare.com/data/pricemulti';
+
+    angular.extend($s, {
+
+        items: {
+            'DOGE': {
+                trades: [
+                    {
+                        amount: 12000,
+                        price: 0.0012,
+                        currency: 'EUR'
+                    }
+                ]
+            }
+        }
+
+    });
+
+    $http.get(apiLink, {params: {fsyms: 'ETH,DASH,DOGE', tsyms: 'BTC,USD,EUR'}}).then(
+        function(response) {
+            console.log(response.data);
+            angular.forEach(response.data, function(data, code) {
+                if (angular.isDefined($s.items[code])) {
+                    $s.items[code].$$currentPrice = data;
+                }
+            });
+            console.log(response.data);
+        }
+    )
+
+//    function add
+
+}]);
